21cf4b54fd29fc8f1a619f2dbb21d5b9be6c5712
margaretvv <brykmargaret@gmail.com>
2024-11-17 14:31:16 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+import sys
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow
+from ui import Ui_MainWindow
+from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
+
+
+class Widget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.configure()
+        self.media=QMediaPlayer(self)
+        self.media.setVideoOutput(self.ui.video)
+
+
+    def configure(self):
+        self.ui.start.clicked.connect(self.media_play)
+        self.ui.stop.clicked.connect(self.media_stop)
+        self.ui.calendar.selectionChanged.connect(self.get_date)
+
+    def get_date(self):
+        self.media_stop()
+        print(self.ui.calendar.selectedDate())
+        day=str(self.ui.calendar.selectedDate().day())
+        self.media.setMedia(QUrl.fromLocalFile(f'Video\\{day}.avi'))
+
+    def media_play(self):
+        self.media_play()
+
+    def media_stop(self):
+        self.media_stop()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Widget()
+    ex.show()
+    sys.exit(app.exec_())
